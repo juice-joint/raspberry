662aea2498d66a4ef04096d4eb857a3f6d8fc98e
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2025-02-23 18:03:47 -0800

    rename: src-tauri -> tauri

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
+      // 3. tell vite to ignore watching `tauri`
       ignored: ["**/tauri/**"],
     },
   },
