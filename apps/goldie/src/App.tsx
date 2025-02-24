cf5b55774a57e645636dda99c3a61429f38c7e7b
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2025-02-23 22:50:21 -0800

    add turborepo

--- apps/goldie/src/App.tsx
+++ apps/goldie/src/App.tsx
@@ -1,3 +1,5 @@
+import { invoke } from "@tauri-apps/api/core";
+import React from "react";
 import { useCurrentSong } from "./api/queries/useCurrentSong";
 import { useEventSource } from "./api/sse/useEventSource";
 import { ErrorScreen } from "./components/error/component";
@@ -5,9 +7,18 @@ import QRCodeBanner from "./components/qr-code/component";
 import { Queue } from "./components/queue/component";
 import { Splash } from "./components/splash/component";
 import { VideoPlayer } from "./components/video-player";
+
 function App() {
   const currentSong = useCurrentSong();
   const { error } = useEventSource();
+
+  React.useEffect(() => {
+    // example invocation
+    invoke<string>("greet", { name: "john" })
+      .then((response: string) => console.log(response))
+      .catch((e: Error) => console.error(e));
+  }, []);
+
   if (error) {
     return <ErrorScreen />;
   }
